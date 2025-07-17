b05fdf9523bf89bc22f7ae9380fd9c98e440b3d8
makurea <makurea@outlook.com>
2025-07-17 16:11:54 +0300

    beforeeach

--- src/main/java/dev/makurea/testanalyzer/TestTimerExtension.java
+++ src/main/java/dev/makurea/testanalyzer/TestTimerExtension.java
@@ -2,25 +2,39 @@ package dev.makurea.testanalyzer;
 
 import org.junit.jupiter.api.extension.*;
 
-public class TestTimerExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
+public class TestTimerExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback,
+    BeforeEachCallback, AfterEachCallback {
 
-  private static final String START_TIME = "start-time";
+  private static final String START_TIME_TEST = "start-time-test";
+  private static final String START_TIME_BEFORE_EACH = "start-time-before-each";
+  private static final String START_TIME_AFTER_EACH = "start-time-after-each";
+
+  @Override
+  public void beforeEach(ExtensionContext context) {
+    context.getStore(ExtensionContext.Namespace.create(getClass()))
+        .put(START_TIME_BEFORE_EACH, System.currentTimeMillis());
+  }
+
+  @Override
+  public void afterEach(ExtensionContext context) {
+    long start = context.getStore(ExtensionContext.Namespace.create(getClass()))
+        .remove(START_TIME_BEFORE_EACH, long.class);
+    long duration = System.currentTimeMillis() - start;
+    System.out.printf("[TestTimer] %s BeforeEach executed in %d ms%n", context.getDisplayName(), duration);
+  }
 
   @Override
   public void beforeTestExecution(ExtensionContext context) {
-    long startTime = System.currentTimeMillis();
     context.getStore(ExtensionContext.Namespace.create(getClass()))
-        .put(START_TIME, startTime);
+        .put(START_TIME_TEST, System.currentTimeMillis());
   }
 
   @Override
   public void afterTestExecution(ExtensionContext context) {
-    long endTime = System.currentTimeMillis();
-    long startTime = context.getStore(ExtensionContext.Namespace.create(getClass()))
-        .remove(START_TIME, long.class);
-    long duration = endTime - startTime;
-
-    String testName = context.getDisplayName();
-    System.out.printf("[TestTimer] %s executed in %d ms%n", testName, duration);
+    long start = context.getStore(ExtensionContext.Namespace.create(getClass()))
+        .remove(START_TIME_TEST, long.class);
+    long duration = System.currentTimeMillis() - start;
+    System.out.printf("[TestTimer] %s Test method executed in %d ms%n", context.getDisplayName(), duration);
   }
 }
+
