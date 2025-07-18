6cee3d224271ef7b600868daf73c37a28cb88a65
makurea <makurea@outlook.com>
2025-07-18 11:19:15 +0300

    Html refactoring code

--- src/main/java/dev/makurea/testanalyzer/report/ReportGenerator.java
+++ src/main/java/dev/makurea/testanalyzer/report/ReportGenerator.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.makurea.testanalyzer.core.TestResult;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -34,8 +33,10 @@ public class ReportGenerator {
       File htmlFile = new File(dir, HTML_FILENAME);
       String htmlContent = loadHtmlTemplate(JSON_FILENAME);
 
-      try (BufferedWriter writer = Files.newBufferedWriter(htmlFile.toPath(), StandardCharsets.UTF_8,
+      try (BufferedWriter writer = Files.newBufferedWriter(htmlFile.toPath(),
+          StandardCharsets.UTF_8,
           StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+        writer.write(htmlContent);
       }
 
       System.out.println("[TestAnalyzer] Отчёт создан: " + htmlFile.getAbsolutePath());
@@ -50,9 +51,10 @@ public class ReportGenerator {
     try (InputStream is = getClass().getResourceAsStream(HTML_TEMPLATE_PATH);
         Scanner scanner = new Scanner(is, StandardCharsets.UTF_8.name())) {
       String template = scanner.useDelimiter("\\A").next();
-      return template.replace("", jsonFileName);
+      return template.replace("{{JSON_FILENAME}}", jsonFileName);
     } catch (NullPointerException e) {
-      throw new IOException("HTML template not found: " + HTML_TEMPLATE_PATH + ". Make sure it's in the same package as ReportGenerator.java or adjust path.", e);
+      throw new IOException("HTML template not found: " + HTML_TEMPLATE_PATH
+          + ". Make sure it's in the same package as ReportGenerator.java or adjust path.", e);
     }
   }
 }
